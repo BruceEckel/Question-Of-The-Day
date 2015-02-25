3d23f40294b2b195cdd639fd7437aa6b6f2cc6c0
Brad Kizzort <bkizzort@yahoo.com>
2015-02-25 17:33:52 -0500

    Created function in DailyQuestions to return todaysQuestion

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 import DailyQuestions
-dummy = dict(first_question=DailyQuestions.QUESTIONS[0][0],
-		second_question=DailyQuestions.QUESTIONS[0][1])
 
 def create_app():
   app = Flask(__name__)
@@ -18,11 +16,13 @@ def index():
 
 @app.route("/first_question")
 def first_question():
-    return render_template('first_question.html', questions = dummy)
+    return render_template('first_question.html', 
+    	questions = DailyQuestions.todaysQuestion())
 
 @app.route("/second_question")
 def second_question():
-    return render_template('second_question.html', questions = dummy)
+    return render_template('second_question.html',
+    	questions = DailyQuestions.todaysQuestion())
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
